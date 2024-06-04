aa37379189d9988e657bf4dc22ca21971709abed
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-06-04 17:19:19 +0530

    Dockerfile for catalogue

--- run.sh
+++ run.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
-
-for parameter in $PARAMETERS; do
+for parameter in catalogue.prod.MONGO_URL,MONGO_URL catalogue.prod.DOCUMENTDB,DOCUMENTDB; do
   ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
   app_parameter=$(echo $parameter | awk -F , '{print $2}')
-  echo export ${app_parameter}=\"$(aws get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/common/parameters
+  echo export ${app_parameter}=\"$(aws get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\"
 done
 
 
-cat /common/parameters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for parameter in $PARAMETERS; do
+#  ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
+#  app_parameter=$(echo $parameter | awk -F , '{print $2}')
+#  echo export ${app_parameter}=\"$(aws get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/common/parameters
+#done
+#
+#
+#cat /common/parameters
 
